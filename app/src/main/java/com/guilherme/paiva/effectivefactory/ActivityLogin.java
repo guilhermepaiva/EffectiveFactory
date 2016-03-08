d5c2e81319ccbcbf629b4ab3594975570b2d5336
Guilherme Leite Moreira de Paiva <guilherm@cinmoto.intranet>
2016-03-08 12:26:05 -0300

    improvement when reading a nfc tag

--- app/src/main/java/com/guilherme/paiva/effectivefactory/ActivityLogin.java
+++ app/src/main/java/com/guilherme/paiva/effectivefactory/ActivityLogin.java
@@ -1,19 +1,22 @@
 package com.guilherme.paiva.effectivefactory;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 /**
@@ -34,6 +37,11 @@ public class ActivityLogin extends Activity {
     private StringBuffer ptoToBeInserted = new StringBuffer();
     private String toBeInserted;
 
+    Tag myTag;
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +51,90 @@ public class ActivityLogin extends Activity {
         textViewMessageNoneEmployeeLogged = (TextView) findViewById(R.id.messageNoneEmployeeLogged);
         listView = (ListView) findViewById(R.id.listViewLogin);
 
-        updateTextViews();
+        //updateTextViews();
+
+        context = this;
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "Este smartphone nao suporta NFC.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        readFromIntent(getIntent());
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
 
     }
 
-    private  void updateTextViews() {
+
+    /******************************************************************************
+     **********************************Read From NFC Tag***************************
+     ******************************************************************************/
+    private void readFromIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+//        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
+
+        if (verifyTagOperativePost(text)){
+            Toast.makeText(ActivityLogin.this, "Leu tag de Posto Operativo", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(ActivityLogin.this, "Leu tag de Funcionario", Toast.LENGTH_LONG).show();
+        }
+
+        //Toast.makeText(ActivityLogin.this, "Conteudo da tag NFC: " + text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        readFromIntent(intent);
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
+    }
+
+    private boolean verifyTagOperativePost(String textReceived){
+        if (textReceived.startsWith("Posto")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /*private  void updateTextViews() {
 
 
 
@@ -131,6 +218,6 @@ public class ActivityLogin extends Activity {
         super.onResume();
         updateTextViews();
         handleNfcIntent(getIntent());
-    }
+    }*/
 
 }
